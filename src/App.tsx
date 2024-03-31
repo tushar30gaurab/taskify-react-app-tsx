3b386bf7672fca03af3fc53da9a767939769df13
Tushar Gourav <tushar30gaurab@gmail.com>
2024-04-01 00:38:18 +0530

    final commit for 01042024

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './App.css';
+import InputField from './components/InputField';
+import { Todo } from './model';
+import TodoList from './components/TodoList';
+
+const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo) {
+      setTodos([...todos, {id: Date.now(), todo, isDone: false}]);
+      setTodo("");
+    }
+  };
+  console.log(todos);
+  return (
+    <div className="App">
+    <span className="heading">Taskify</span> 
+    <InputField todo = {todo} setTodo = {setTodo} handleAdd = {handleAdd} />
+    <TodoList todos={todos} setTodos={setTodos}/>
+    {/* {todos.map((t) => (
+      <li>{t.todo}</li>
+    ))} */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/model.ts
@@ -0,0 +1,8 @@
+export interface Todo {
+    id: number;
+    todo: string;
+    isDone: boolean;
+}
+
+// https://img.freepik.com/free-photo/crumpled-yellow-paper-background-close-up_60487-2390.jpg?size=626&ext=jpg
+//   #2f74c0;
